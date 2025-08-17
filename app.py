80bb2fdafd6d5e0086c4559f6ca3e25b05c1c161
Neelam Jaswanth <142784800+JASWANTHNEELAM@users.noreply.github.com>
2025-08-17 16:11:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def after_request(response):
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=False)
 
